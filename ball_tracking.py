061185ab64a3ae04e8dd05c61f0047fccc1d4a81
3lswear <r.malyavsky@protonmail.com>
2021-05-21 21:12:16 +0300

    found

--- ball_tracking.py
+++ ball_tracking.py
@@ -4,69 +4,167 @@ import argparse
 import imutils
 import cv2
 
-ap = argparse.ArgumentParser()
+'''ap = argparse.ArgumentParser()
 ap.add_argument("-v",
 	help="path to the (optional) video file")
 ap.add_argument("-b", "--buffer", type=int, default=64,
+ap.add_argument("-b", "--buffer", type=int, default=64,
 	help="max buffer size")
-args = vars(ap.parse_args())
+args = vars(ap.parse_args())'''
+#синий
+#lower1 = (61, 138, 152)
+#upper1 = (86, 255, 255)
 
-greenLower = (33, 155, 117)
-greenUpper = (122, 255, 255)
-pts = deque(maxlen=args["buffer"])
+#lower2 = (33, 129, 157)
+#upper2 = (119, 255, 255)
+
+#lower1 = (0, 131, 145)
+#upper1 = (63, 197, 192)
+#мандарин
+#lower2 = (0, 131, 145)
+#upper2 = (63, 197, 192)
+#желтый
+lower3 = (63, 51, 0)
+upper3 = (96, 255, 255)
+#розовый
+lower4 = (124, 42, 159)
+upper4 = (183, 185, 222)
+#pts = deque(maxlen=args["buffer"])
+
+
+lower1 = (33, 155, 117)
+upper1 = (122, 255, 255)
+
+lower2 = (0, 131, 145)
+upper2 = (63, 197, 192)
 
 camera = cv2.VideoCapture(0)
 
+
+
 # keep looping
-while True:
+while True :
 	# grab the current frame
-	(grabbed, frame) = camera.read()
+	(grabbed, frame1) = camera.read()
+	#(grabbed, frame2) = camera.read()
 
 	# if we are viewing a video and we did not grab a frame,
 	# then we have reached the end of the video
-	if args.get("video") and not grabbed:
-		break
+	#if args.get("video") and not grabbed:
+		#break
 
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
 	#frame = #imutils.resize(frame, width=600)
 	# blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
+	#hsv2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2HSV)
 
 	# construct a mask for the color "green", then perform
 	# a series of dilations and erosions to remove any small
 	# blobs left in the mask
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
+	mask1 = cv2.inRange(hsv, lower1, upper1)
+	mask1 = cv2.erode(mask1, None, iterations=2)
+	mask1 = cv2.dilate(mask1, None, iterations=2)
+	#print("      ", mask1)
+	mask2 = cv2.inRange(hsv, lower2, upper2)
+	mask2 = cv2.erode(mask2, None, iterations=2)
+	mask2 = cv2.dilate(mask2, None, iterations=2)
+
+	mask3 = cv2.inRange(hsv, lower3, upper3)
+	mask3 = cv2.erode(mask3, None, iterations=2)
+	mask3 = cv2.dilate(mask3, None, iterations=2)
+
+	mask4 = cv2.inRange(hsv, lower4, upper4)
+	mask4 = cv2.erode(mask4, None, iterations=2)
+	mask4 = cv2.dilate(mask4, None, iterations=2)
+	#print(mask2)
 	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+	mask = mask1 + mask2 + mask3 + mask4
+	cnts1 = cv2.findContours(mask1.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)[-2]
-	center = None
+	cnts2 = cv2.findContours(mask2.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	cnts3 = cv2.findContours(mask3.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	cnts4 = cv2.findContours(mask4.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+	center1 = None
+	center2 = None
+	center3 = None
+	center4 = None
 
 	# only proceed if at least one contour was found
-	if len(cnts) > 0:
+	if len(cnts1) > 0  and len(cnts2) > 0 and len(cnts3) > 0 and len(cnts4) > 0 :
 		# find the largest contour in the mask, then use
 		# it to compute the minimum enclosing circle and
 		# centroid
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-		print(center)
+		c1 = max(cnts1, key=cv2.contourArea)
+		#print(cnts2)
+		c2 = max(cnts2, key=cv2.contourArea)
+
+		c3 = max(cnts3, key=cv2.contourArea)
+
+		c4 = max(cnts4, key=cv2.contourArea)
+
+		((x1, y1), radius1) = cv2.minEnclosingCircle(c1)
+
+		((x2, y2), radius2) = cv2.minEnclosingCircle(c2)
+
+		((x3, y3), radius3) = cv2.minEnclosingCircle(c3)
+
+		((x4, y4), radius4) = cv2.minEnclosingCircle(c4)
+
+		M1 = cv2.moments(c1)
+
+		M2 = cv2.moments(c2)
+
+		M3 = cv2.moments(c3)
+
+		M4 = cv2.moments(c4)
+		center1 = (int(M1["m10"] / M1["m00"]), int(M1["m01"] / M1["m00"]))
+
+		center2 = (int(M2["m10"] / M2["m00"]), int(M2["m01"] / M2["m00"]))
+
+		center3 = (int(M3["m10"] / M3["m00"]), int(M3["m01"] / M3["m00"]))
+
+		center4 = (int(M4["m10"] / M4["m00"]), int(M4["m01"] / M4["m00"]))
+		print(center1, "            ", center2, "            ",center3 ,"            ", center4)
 		# only proceed if the radius meets a minimum size
-		if radius > 10:
+		if radius1 > 10:
 			# draw the circle and centroid on the frame,
 			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius),
-				(0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
+			cv2.circle(frame1, (int(x1), int(y1)), int(radius1),
+				(255, 255, 0), 2)
+			#Красные точки
+			#cv2.circle(frame1, center1, 5, (0, 0, 0), -1)
+			cv2.circle(frame1, center1, 5, (0, 0, 255), -1)
+			#Поменять
+			if radius2 > 10:
+				# draw the circle and centroid on the frame,
+				# then update the list of tracked points
+				cv2.circle(frame1, (int(x2), int(y2)), int(radius2),
+					(255, 255, 0), 2)
+				cv2.circle(frame1, center2, 5, (0, 0, 255), -1)
+				if radius3 > 10:
+					# draw the circle and centroid on the frame,
+					# then update the list of tracked points
+					cv2.circle(frame1, (int(x3), int(y3)), int(radius3),
+							   (255, 255, 0), 2)
+					cv2.circle(frame1, center3, 5, (0, 0, 255), -1)
+					if radius4 > 10:
+						# draw the circle and centroid on the frame,
+						# then update the list of tracked points
+						cv2.circle(frame1, (int(x4), int(y4)), int(radius4),
+								   (255, 255, 0), 2)
+						cv2.circle(frame1, center4, 5, (0, 0, 255), -1)
 	# update the points queue
-	pts.appendleft(center)
+	#pts.appendleft(center)
 	# loop over the set of tracked points
 	xrange = range
+	'''
 	for i in xrange(1, len(pts)):
 		# if either of the tracked points are None, ignore
 		# them
@@ -75,15 +173,21 @@ while True:
 
 		# otherwise, compute the thickness of the line and
 		# draw the connecting lines
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+		thickness = 0
+		#int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
 		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+		'''
+
+
+
 
 	# show the frame to our screen
-	cv2.imshow("Frame", frame)
+	cv2.imshow("VIDEO", frame1)
+	#cv2.imshow("Frame", frame2)
 	key = cv2.waitKey(1) & 0xFF
 
 	# if the 'q' key is pressed, stop the loop
-	if key == ord("q"):
+	if key == ord("z") or key == ord("q"):
 		break
 
 # cleanup the camera and close any open windows
--- test.py
+++ test.py
@@ -5,25 +5,20 @@ cap = cv2.VideoCapture(0)
 
 while(1):
 
-# Take each frame
+    # Take each frame
     _, frame = cap.read()
 
-# Convert BGR to HSV
+    # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-# define range of blue color in HSV
+    # define range of blue color in HSV
     lower_blue = np.array([110,50,50])
     upper_blue = np.array([130,255,255])
 
-    lower_green = np.array([50, 50, 120])
-    upper_green = np.array([70, 255, 255])
-    green_mask = cv2.inRange(hsv, lower_green, upper_green) # I have the Green threshold image.
-
     # Threshold the HSV image to get only blue colors
-    blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    mask = blue_mask + green_mask
-
-# Bitwise-AND mask and original image
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    #greenMask = inRange(hsv, lower_blue, upper_blue)
+    # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
 
     cv2.imshow('frame',frame)
